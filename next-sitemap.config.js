5fe02e32f032e1a3647decc2589bbf3afbc1c668
Christian Baumann <33223146+chbauman@users.noreply.github.com>
2026-05-14 10:52:39 +0200

    More SEO improvements

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://yonostreetband.ch",
+  generateRobotsTxt: true,
+  outDir: "./out", // Important for static export!
+};
